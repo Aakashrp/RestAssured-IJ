73d1752945426acdc1505f05f1895fe68b2a589f
Aakashrp <93774822+Aakashrp@users.noreply.github.com>
2024-06-21 01:51:26 +0530

    Update Runner.java

--- src/test/java/com/Runner/Runner.java
+++ src/test/java/com/Runner/Runner.java
@@ -1,17 +1,1 @@
-package com.Runner;
 
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "E:\\My Projects\\All Projects\\API Testing\\src\\main\\resources\\features",
-        glue = "stepDefinition",
-       // tags = "@Testcase or @Testtwo or @New",
-)
-
-public class Runner {
-
-}
